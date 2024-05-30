ad5a0521828efefb2163e58f0b8c2c130ecfcdfc
Beccatoni <rbccmtni1@gmail.com>
2024-05-30 21:00:53 +0200

    getter in an object literal

--- getObjs.js
+++ getObjs.js
@@ -1,1 +1,17 @@
-let meeting = 
+// using getter in an object literal
+
+let meeting = {
+    attendees: [],
+    add(attendee){
+        console.log(`${attendee} joined the meeting.`);
+        this.attendees.push(attendee);
+        return this;
+    },
+    get latest(){
+        let count = this.attendees.length;
+        return count == 0 ? undefined : this.attendees[count - 1];
+    }
+}
+
+meeting.add('john').add('jane').add('Peter');
+console.log(`The latest attendee is ${meeting.latest}.`);

4789b3ca171c462bd5f30540b34ee019cd8db487
Beccatoni <rbccmtni1@gmail.com>
2024-06-02 08:12:56 +0200

    comment to explain why we use the let keyword

--- explanations/closureInLoops.js
+++ explanations/closureInLoops.js
@@ -3,3 +3,5 @@ for(var i = 1; i <= 4; i++){
         console.log(`After ${i} ${i>1? 'Seconds':'Second'}: ${i}`)
     }, i * 1000);
 }
+// to solve the excution problem to stop displaying the same message use the let keyword or use the IIFE
+

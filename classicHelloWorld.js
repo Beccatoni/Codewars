b0a8fc75dcaee696c52d81671a9e645fdb4be0b7
Beccatoni <rbccmtni1@gmail.com>
2024-06-25 09:05:24 +0200

    classic hello world

---
+++ classicHelloWorld.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57036f007fd72e3b77000023/train/javascript
+
+class Solution{
+    static main(message){
+        console.log('Hello World!');
+    }
+}
+
+Solution.main()

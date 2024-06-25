031701b1021eef9dca5686f5889a3ceeb386dc24
Beccatoni <rbccmtni1@gmail.com>
2024-06-25 13:03:15 +0200

    finding the geometric mean of numbers in an array given the arithmetic mean and the array of numbers missing one number

--- geometricMean.js
+++ geometricMean.js
@@ -1,11 +1,9 @@
 // https://www.codewars.com/kata/56ebcea1b9d927f9bf000544/train/javascript
 
-function geometricMean(arr){
-    var gm;
-
-    gm = Math.pow((arr.reduce((a,b) => a*Math.abs(b),1)), 1/arr.length);
-
-    return gm;
-}
-
-console.log(geometricMean([2, 3, 4, 6, -5]));
+function geo_mean(nums, arith_mean) {
+    let sum = arith_mean * (nums.length + 1);
+    let missing_num = sum - nums.reduce((a,c)=> a + c, 0);
+    nums.push(missing_num);
+     let geometric_mean = nums.reduce((a,b)=> a * b, 1) **( 1/nums.length)
+    return geometric_mean;
+  }

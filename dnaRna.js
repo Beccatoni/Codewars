0cf7c8c48c8431e0edff9337cda4c9b0d20a6aca
Beccatoni <rbccmtni1@gmail.com>
2024-07-08 11:20:49 +0200

    simple way to solve the DNATORNA

--- dnaRna.js
+++ dnaRna.js
@@ -1,13 +1,13 @@
 function DNAtoRNA(dna) {
     // create a function which returns an RNA sequence from the given DNA sequence
-    let rna = [];
-    for(let letter of dna){
-      if(letter !== 'T'){
-        rna.push(letter);
-      } else{
-        rna.push('U')
-      }
-    }
-    return rna.join('')
-  
-  }
+    // let rna = [];
+    // for(let letter of dna){
+    //   if(letter !== 'T'){
+    //     rna.push(letter);
+    //   } else{
+    //     rna.push('U')
+    //   }
+    // }
+    // return rna.join('')
+    return dna.replace(/T/g, 'U')
+}

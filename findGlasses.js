5dece872694d92401832e19858d7f04faeea5870
Beccatoni <rbccmtni1@gmail.com>
2024-08-01 11:51:01 +0200

    matching glasses pattern

---
+++ findGlasses.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/6512c786a07f6a000fe7a274/train/javascript
+
+function findGlasses(arr){
+    let regEx = /O-+O/
+
+    for(let i = 0; i < arr.length; i++){
+        if(regEx.test(arr[i])){
+            return i 
+        }
+    }
+
+    return undefined
+}

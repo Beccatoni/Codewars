4b2ec357b244cb6347809b7d608641e3fbcc0709
Beccatoni <rbccmtni1@gmail.com>
2024-08-12 18:27:49 +0200

    password validator

---
+++ validatePasswords.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5838719c6754d618240000ff/train/javascript
+
+let storedPassword = []
+
+var signIn = function(newPassword){
+    storedPassword.push(newPassword);
+    return storedPassword;
+}
+
+var logIn = function (password) {
+    for (let i = 0; i < storedPassword.length; i++) {
+        if (password === storedPassword[i]) {
+            return true
+        }
+        return false
+        
+    }
+}
